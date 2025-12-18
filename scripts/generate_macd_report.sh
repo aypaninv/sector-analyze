c704ccb4bee23a589221001a186801339627d0b5
aypaninv <aypaninv@users.noreply.github.com>
2025-12-18 01:52:13 +0000

    deploy: 2daa43d842232d509762cb84f7449eb3f2bb0c75

--- scripts/generate_macd_report.sh
+++
@@ -1,6 +0,0 @@
-python src/main/yfinance/yfinance_4hours_fetcher.py
-python src/main/yfinance/yfinance_daily_fetcher.py
-python src/main/yfinance/yfinance_weekly_fetcher.py
-
-python src/main/macd/macd_calculator.py
-python src/main/macd/macd_report.py
